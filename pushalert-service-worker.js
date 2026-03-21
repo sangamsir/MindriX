dcd0a8d9a153620a81e6e235d5bd2d0ab7ed9658
sangamsir <sangamsir777@gmail.com>
2026-03-21 13:22:26 +0530

    Add files via upload

---
+++ pushalert-service-worker.js
@@ -0,0 +1,5 @@
+// PushAlert Service Worker
+// File name: pushalert-service-worker.js
+// GitHub repo ki ROOT mein upload karo (index.html ke saath)
+
+importScripts('https://cdn.pushalert.co/sw/pushalert-sw.js');
