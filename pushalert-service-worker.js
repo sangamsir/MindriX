a3fa36b2701e6ef893ecc23e627480d8692e2f42
sangamsir <sangamsir777@gmail.com>
2026-03-28 07:33:51 +0530

    Delete pushalert-service-worker.js

--- pushalert-service-worker.js
+++
@@ -1,5 +0,0 @@
-// PushAlert Service Worker
-// File name: pushalert-service-worker.js
-// GitHub repo ki ROOT mein upload karo (index.html ke saath)
-
-importScripts('https://cdn.pushalert.co/sw/pushalert-sw.js');
